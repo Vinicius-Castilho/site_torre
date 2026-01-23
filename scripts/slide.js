8f16aea124c536ea1271a7b35097c6ea3f84aaae
Vinicius Castilho <142710734+Vinicius-Castilho@users.noreply.github.com>
2026-01-23 08:29:54 -0300

    Add files via upload

---
+++ scripts/slide.js
@@ -0,0 +1,17 @@
+// const emblaNode = document.querySelector('#historia-imagem');
+// const prevBtn = emblaNode.querySelector('.embla__prev');
+// const nextBtn = emblaNode.querySelector('.embla__next');
+
+// const options = { loop: true, duration: 25 };
+// const plugins = [EmblaCarouselFade()]; 
+
+// const emblaApi = EmblaCarousel(emblaNode, options, plugins);
+
+// // Listeners para os botões manuais
+// prevBtn.addEventListener('click', () => emblaApi.scrollPrev(), false);
+// nextBtn.addEventListener('click', () => emblaApi.scrollNext(), false);
+
+// // Autoplay (Opcional: você pode remover se quiser apenas manual)
+// setInterval(() => {
+//   if (emblaApi) emblaApi.scrollNext();
+// }, 5000);
